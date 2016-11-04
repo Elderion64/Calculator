bef2a314e3b3dd2db42a16c996f59bc12c0dd741
Elderion64 <antoine.barachet@viacesi.fr>
2016-11-04 14:37:13 +0100

    Compute a soustraction

--- main.c
+++ main.c
@@ -5,11 +5,11 @@
 
 int addition (int a, int b)
 {
-    return a+b;
+    return a-b;
 }
 
 int main()
 {
-    printf("L’addition de a et de b vaut%d\n", addition(1,4));
+    printf("La soustraction de a et de b vaut%d\n", addition(1,4));
     return 0;
 }
